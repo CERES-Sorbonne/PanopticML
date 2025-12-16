ce19f6361939db61c545309c4b636ab1bc16aa85
Félix <someonefefe@gmail.com>
2025-12-16 17:33:30 +0100

    add possibility to predownload models

--- panopticml/__init__.py
+++ panopticml/__init__.py
@@ -1,3 +1,16 @@
+import logging
+
+from panopticml.compute.transformer import get_transformer, type_to_class_mapping
+
 from .panoptic_ml import PanopticML
 
 plugin_class = PanopticML
+
+def init_model(model_name="clip"):
+    """
+    utility function that allows to download a model without starting panoptic and importing images
+    :param model_name: name of the model, default to clip
+    """
+    logging.info("Initializing model clip")
+    model = type_to_class_mapping[model_name]("openai/clip-vit-base-patch32")
+    return model
